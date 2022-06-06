7561a6617ee997038fe7644380a3b89a67eb54f7
fstipo <fstipanov@gmail.com>
2022-06-06 15:10:49 +0200

    err.message

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ const getTodos = async () => {
   return data;
 };
 
-console.log(1);
-console.log(2);
 getTodos()
   .then((data) => console.log('resolved:', data))
-  .catch((err) => console.log('rejected:', err));
-console.log(3);
-console.log(4);
+  .catch((err) => console.log('rejected:', err.message));
